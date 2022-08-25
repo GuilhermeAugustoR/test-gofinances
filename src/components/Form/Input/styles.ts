4e587aff68a08dc3655943ca7afb13ce974dc03d
GuilhermeAugustoR <guilhermereiscampos23@gmail.com>
2022-08-25 10:00:50 -0300

    ✅ Criando testes e erro no teste do input

--- src/components/Form/Input/styles.ts
+++ src/components/Form/Input/styles.ts
@@ -1,6 +1,10 @@
-import styled from 'styled-components/native';
-import { TextInput } from 'react-native';
-import { RFValue } from 'react-native-responsive-fontsize';
+import styled from "styled-components/native";
+import { TextInput } from "react-native";
+import { RFValue } from "react-native-responsive-fontsize";
+
+interface Props {
+  active: boolean;
+}
 
 export const Container = styled(TextInput)`
   width: 100%;
@@ -8,10 +12,17 @@ export const Container = styled(TextInput)`
 
   font-family: ${({ theme }) => theme.fonts.regular};
   font-size: ${RFValue(14)}px;
-  
+
   color: ${({ theme }) => theme.colors.text_dark};
   background-color: ${({ theme }) => theme.colors.shape};
   border-radius: 5px;
 
   margin-bottom: 8px;
+
+  ${({ active, theme }) =>
+    active &&
+    css`
+      border-width: 3px;
+      border-color: ${theme.colors.attention};
+    `}
 `;
